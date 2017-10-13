613755ad6f2202d36366cb65b96addd41c139567
zhennann <zhennann@qq.com>
2017-10-13 16:51:36 +0800

    milestone2

--- boilerplate/src/front/vuerouter/main.js
+++ boilerplate/src/front/vuerouter/main.js
@@ -8,6 +8,7 @@ import App from './app';
 import Home from './home';
 import config from '../config/config.js';
 import locales from '../config/locales.js';
+import mock from './mock/mock.js';
 
 let Vue;
 
@@ -16,6 +17,9 @@ function install(_Vue, cb) {
   if (Vue) return console.error('already installed.');
 
   Vue = _Vue;
+
+  // mock
+  Vue.use(mock);
 
   // router
   Vue.use(Router);
